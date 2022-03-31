32daea897e93809a0329a94beabb0b3b4881bf07
Anna Mosolova <39431144+anya-bel@users.noreply.github.com>
2022-03-31 19:41:29 +0200

    first model added

---
+++ scripts/model.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, src_vocab_size, hidden_size, emb_hidden_size, dropout_rate):
+        super(EncoderRNN, self).__init__()
+        self.src_vocab_size = src_vocab_size
+        self.hidden_size = hidden_size
+        self.emb_hidden_size = emb_hidden_size
+        self.dropout_rate = dropout_rate
+        self.embedding = nn.Embedding(self.src_vocab_size, self.emb_hidden_size)
+        self.gru = nn.GRU(self.emb_hidden_size, self.hidden_size, batch_first=True)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, sentence):
+        sent_emb = self.dropout(self.embedding(sentence))
+        output, hidden = self.gru(sent_emb)
+        return output, hidden
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, tgt_vocab_size, hidden_size, emb_hidden_size, dropout_rate):
+        super(DecoderRNN, self).__init__()
+        self.tgt_vocab_size = tgt_vocab_size
+        self.hidden_size = hidden_size
+        self.emb_hidden_size = emb_hidden_size
+        self.dropout_rate = dropout_rate
+        self.embedding = nn.Embedding(self.tgt_vocab_size, self.emb_hidden_size)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.gru = nn.GRU(self.emb_hidden_size, self.hidden_size, batch_first=True)
+        self.out = nn.Linear(self.hidden_size, self.tgt_vocab_size)
+
+    def forward(self, word, hidden):
+        batch_size = word.shape[0]
+        word_emb = self.dropout(self.embedding(word).view(batch_size, 1, -1))
+        output, hidden = self.gru(word_emb, hidden)
+        output = self.out(output.view(batch_size, -1))
+        return output, hidden
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(Seq2Seq, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.trg_vocab_size = decoder.tgt_vocab_size
+
+    def forward(self, src_sentence, tgt_len):
+        batch_size = src_sentence.shape[0]
+        pred_probas = torch.zeros(tgt_len, batch_size, self.trg_vocab_size)
+        pred_probas[0, :, 2] = 1
+        prev_word = torch.ones(batch_size).int() * 2
+
+        output, hidden = self.encoder(src_sentence)
+
+        for t in range(1, tgt_len):
+            pred_proba_t, hidden = self.decoder(prev_word, hidden)
+            pred_probas[t, :, :] = pred_proba_t
+            prev_word = pred_proba_t.argmax(-1)
+
+        return pred_probas
---
+++ scripts/utils.py
@@ -0,0 +1,64 @@
+import torch
+from nltk.translate import bleu_score
+from tqdm.auto import tqdm
+
+
+def evaluate_val(model, dataset, val_dataloader):
+    references = []
+    hypotheses = []
+    for el in val_dataloader:
+        for tgt_sent, hyp_sent in zip(el[1], model(el[0], 41)[1:].argmax(0)):
+            reference = dataset.get_sentence_tgt(tgt_sent)
+            hypothesis = dataset.get_sentence_tgt(hyp_sent)
+            references.append([reference])
+            hypotheses.append(hypothesis)
+    return bleu_score.corpus_bleu(references, hypotheses)
+
+
+def training(model, train_dataloader, val_dataloader, dataset, num_epochs, loss_fn, optimizer, device='cpu',
+             verbose=True):
+    model.train()
+    model.to(device)
+    loss_total = []
+    best_bleu = 0
+    best_epoch = 0
+
+    for epoch in range(num_epochs):
+
+        loss_current_epoch = 0
+        t = tqdm(train_dataloader)
+        for i, (src_sentences, tgt_sentences) in enumerate(t):
+            src, tgt = src_sentences.to(device), tgt_sentences.to(device)
+            tgt_len = tgt.shape[1]
+
+            optimizer.zero_grad()
+
+            pred_probas = model(src, tgt_len + 1)
+            output_dim = pred_probas.shape[-1]
+            pred_probas = pred_probas[1:].view(-1, tgt_len, output_dim)
+            pred_probas = pred_probas.view(-1, output_dim)
+            tgt = tgt.view(-1)
+
+            loss = loss_fn(pred_probas, tgt)
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+
+            loss_current_epoch += loss.item()
+            t.set_description(f'loss: {round(float(loss), 3)}')
+            t.refresh()
+
+        loss_total.append(loss_current_epoch)
+        if verbose:
+            with torch.no_grad():
+                model.eval()
+                bleu = evaluate_val(model, dataset, val_dataloader)
+                print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, loss_current_epoch), 'BLEU:', bleu)
+                if bleu > best_bleu:
+                    best_bleu = bleu
+                    best_epoch = epoch
+                model.train()
+        torch.save(model.state_dict(), '../models/seq2seq_model.pt')
+
+    return model, loss_total, best_bleu, best_epoch
